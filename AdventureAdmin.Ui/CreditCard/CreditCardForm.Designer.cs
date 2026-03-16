278d561e725a27c807a733dd836fdfbe202eaa62
JulioS21 <tuemail@example.com>
2026-03-16 14:22:58 -0400

    [add] empezando con credit card

---
+++ AdventureAdmin.Ui/CreditCard/CreditCardForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace AdventureAdmin.Ui.CreditCard
+{
+    partial class CreditCardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(143, 22);
+            label1.TabIndex = 0;
+            label1.Text = "Tipo de tarjeta";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 67);
+            label2.Name = "label2";
+            label2.Size = new Size(152, 22);
+            label2.TabIndex = 1;
+            label2.Text = "Numero Tarjeta";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(12, 111);
+            label3.Name = "label3";
+            label3.Size = new Size(154, 22);
+            label3.TabIndex = 2;
+            label3.Text = "Mes Expiracion";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(12, 146);
+            label4.Name = "label4";
+            label4.Size = new Size(152, 22);
+            label4.TabIndex = 3;
+            label4.Text = "Año Expiracion";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(175, 32);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(131, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(175, 66);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(131, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CustomFormat = "MM";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.Location = new Point(179, 113);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.ShowUpDown = true;
+            dateTimePicker1.Size = new Size(127, 23);
+            dateTimePicker1.TabIndex = 6;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.CustomFormat = "yyyy";
+            dateTimePicker2.Format = DateTimePickerFormat.Custom;
+            dateTimePicker2.Location = new Point(179, 146);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.ShowUpDown = true;
+            dateTimePicker2.Size = new Size(127, 23);
+            dateTimePicker2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.YellowGreen;
+            button1.Font = new Font("Arial Narrow", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(12, 192);
+            button1.Name = "button1";
+            button1.Size = new Size(87, 34);
+            button1.TabIndex = 8;
+            button1.Text = "Guardar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // CreditCardForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(499, 273);
+            Controls.Add(button1);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "CreditCardForm";
+            Text = "Crear Tarjeta de Credito";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private Button button1;
+    }
+}
