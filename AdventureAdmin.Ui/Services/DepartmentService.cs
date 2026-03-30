709554bf6c8ad3ed358c0ebdfe7ae7f420690788
Juniorbp20 <Jbonifacio0707@gmail.com>
2026-03-30 19:16:20 -0400

    cambios en el metodo guardar en e l return cantodad >1

--- AdventureAdmin.Ui/Services/DepartmentService.cs
+++ AdventureAdmin.Ui/Services/DepartmentService.cs
@@ -23,7 +23,7 @@ public class DepartmentService(
     {
         await context.Departments.AddAsync(modelo);
         var cantidad = await context.SaveChangesAsync();
-        return cantidad > 0;
+        return cantidad > 1;
     }
 
     public async Task<List<Data.Models.Department>> GetList(Expression<Func<Data.Models.Department, bool>> criterio)
