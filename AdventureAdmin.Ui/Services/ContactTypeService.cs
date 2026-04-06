b3ff4137e76300147924f3c3abc9dd84692527c9
Leander-Valdez <leandervaldezreyes@gmail.com>
2026-04-05 21:31:56 -0400

    Agregar ConctactTypeService

---
+++ AdventureAdmin.Ui/Services/ContactTypeService.cs
@@ -0,0 +1,47 @@
+﻿using AdventureAdmin.Data.Context;
+using AdventureAdmin.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace AdventureAdmin.Ui.Services;
+
+public class ContactTypeService (AdventureWorksContext context) : Aplicada1.Core.IService<AdventureAdmin.Data.Models.ContactType, int>
+{
+    public async Task<bool> Guardar(Data.Models.ContactType entidad)
+    {
+        if (entidad.ContactTypeId == 0)
+            context.ContactTypes.Add(entidad);
+        else
+            context.ContactTypes.Update(entidad);
+
+        return await context.SaveChangesAsync() > 0;
+    }
+    public async Task<Data.Models.ContactType?> Buscar(int id)
+    {
+        return await context.ContactTypes
+            .FirstOrDefaultAsync(c => c.ContactTypeId == id);
+    }
+
+    public async Task<bool> Eliminar(int id)
+    {
+        var existe = await context.ContactTypes
+           .FirstOrDefaultAsync(c => c.ContactTypeId == id);
+
+        if (existe == null)
+            return false;
+
+        context.ContactTypes.Remove(existe);
+        return await context.SaveChangesAsync() > 0;
+    }
+
+    public async Task<List<Data.Models.ContactType>> GetList(Expression<Func<Data.Models.ContactType, bool>> criterio)
+    {
+        return await context.ContactTypes
+            .Where(criterio)
+            .ToListAsync();
+    }
+}
